607682a297c366deb471ab0b7bb3189239049034
Abdullah-Mehmood-bit <abdullahgithub007@gmail.com>
2023-01-18 16:59:57 -0800

    convert to db serverless function

--- config/database.js
+++ config/database.js
@@ -1,9 +1,17 @@
 const mongoose = require('mongoose');
 
-exports.connectDatabase = () => {
-    mongoose.set('strictQuery', false);
-    mongoose
-      .connect(process.env.MONGO_URL)
-      .then((con) => console.log(`Database Connected: ${con.connection.host}`))
-      .catch((err) => console.log(err));
+exports.connectDatabase = async () => {
+    // mongoose.set('strictQuery', false);
+    // mongoose
+    //   .connect(process.env.MONGO_URL)
+    //   .then((con) => console.log(`Database Connected: ${con.connection.host}`))
+    //   .catch((err) => console.log(err));
+    try {
+      mongoose.set('strictQuery', false);
+      const conn = await mongoose.connect(process.env.MONGO_URL);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
   };
--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const { connectDatabase } = require('./config/database');
 
 /**Connect Database Function Call */
 
-connectDatabase()
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is connected on http://localhost:${process.env.PORT}`)
+connectDatabase().then(()=>{
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is connected on http://localhost:${process.env.PORT}`)
+    })
 })
+
